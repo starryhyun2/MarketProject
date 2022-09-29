cb46c48fe15869904638684d57c744c331254fa7
starryhyun2 <starryhyun2@gmail.com>
2022-09-29 16:12:53 +0900

    나머지도 빨갛게 수정됨

--- Day08/src/main/webapp/J_EX3.js
+++ Day08/src/main/webapp/J_EX3.js
@@ -15,31 +15,34 @@
 }
 
  function Minus(){
+	var minus= document.getElementById('minus');
 	var num1= document.getElementById('num1');
 	var num2= document.getElementById('num2');
 	var ans = document.getElementById('ans');
 	var re_ans = parseInt(num1.value)- parseInt(num2.value);
 	
-	plus.style.background = 'red';
+	minus.style.background = 'red';
 	ans.value=re_ans;
 
 }
  function Multifly(){
+	var multifly=document.getElementById('multifly');
 	var num1= document.getElementById('num1');
 	var num2= document.getElementById('num2');
 	var ans = document.getElementById('ans');
 	var re_ans = parseInt(num1.value)* parseInt(num2.value);
 	
-	plus.style.background = 'red';
+	multifly.style.background = 'red';
 	ans.value=re_ans;
 
 }
  function Devide(){
+	var devide=document.getElementById('devide');
 	var num1= document.getElementById('num1');
 	var num2= document.getElementById('num2');
 	var ans = document.getElementById('ans');
 	
-	plus.style.background = 'red';
+	devide.style.background = 'red';
 	if(parseInt(num1.value)==0){
 		alert("첫 번째 숫자는 0이어서는 안됩니다.");
 		
